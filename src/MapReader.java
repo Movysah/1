0975f5c0a9c908eca9dba62ab19a8c079bd604ff
Movysah <mataveverka@seznam.cz>
2025-02-26 11:28:32 +0100

    0

---
+++ src/MapReader.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MapReader {
+
+
+    public MapReader() {
+
+        stringsOfData = new ArrayList();
+    }
+
+
+    public ArrayList<String> stringsOfData;
+
+
+    public ArrayList<String> getfiletext() {
+
+    }
+
+
+}
